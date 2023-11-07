0ab019a5cde70050638d9735253db013e70a25e2
罗午青 <2972106673@qq.com>
2023-11-07 09:07:42 +0800

    updata

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -11,12 +11,22 @@
       </el-col>
       <div>
         <el-button type="primary" @click="handleAdd">增加</el-button>
-        <el-button type="danger" @click="handleListDel" v-if="multipleSelection.length > 0">删除</el-button>
+        <el-button
+          type="danger"
+          @click="handleListDel"
+          v-if="multipleSelection.length > 0"
+          >删除</el-button
+        >
       </div>
     </div>
     <!--table 表格-->
-    <el-table :data="tableData" style="width: 100%" border @selection-change="handleSelectionChange"
-      ref="multipleTableRef">
+    <el-table
+      :data="tableData"
+      style="width: 100%"
+      border
+      @selection-change="handleSelectionChange"
+      ref="multipleTableRef"
+    >
       <el-table-column type="selection" width="55" />
       <el-table-column prop="name" label="姓名" width="120" />
       <el-table-column prop="email" label="邮件" width="120" />
@@ -25,13 +35,28 @@
       <el-table-column prop="address" label="地址" width="300" />
       <el-table-column fixed="right" label="操作" width="120">
         <template #default="scope">
-          <el-button link type="danger" size="small" @click="handleRowDel(scope.row.id)">删除</el-button>
-          <el-button link type="primary" size="small" @click="handleEdit(scope.row)">编辑</el-button>
+          <el-button
+            link
+            type="danger"
+            size="small"
+            @click="handleRowDel(scope.row.id)"
+            >删除</el-button
+          >
+          <el-button
+            link
+            type="primary"
+            size="small"
+            @click="handleEdit(scope.row)"
+            >编辑</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
     <!--dialog 弹窗-->
-    <el-dialog v-model="dialogFormVisible" :title="dialogType === 'add' ? '新增' : '编辑'">
+    <el-dialog
+      v-model="dialogFormVisible"
+      :title="dialogType === 'add' ? '新增' : '编辑'"
+    >
       <el-form :model="tableForm" label-width="auto">
         <el-form-item label="姓名">
           <el-input v-model="tableForm.name" autocomplete="off" />
@@ -59,86 +84,85 @@
 </template>
 
 <script setup>
-import { ref, watch, onMounted } from 'vue'
-import axios from 'axios'
+import { ref, watch, onMounted } from "vue";
+import axios from "axios";
 //数据
-const queryInput = ref("")
-let tableData = ref([
-
-])
+const queryInput = ref("");
+let tableData = ref([]);
 let tableDataCopy = tableData.value;
-const multipleSelection = ref([]);//多选
-const dialogFormVisible = ref(false);//是否打开
-const formLabelWidth = "80px";//弹出框 标题长度
-const tableForm = ref({})
-const dialogType = ref("add")
+const multipleSelection = ref([]); //多选
+const dialogFormVisible = ref(false); //是否打开
+const formLabelWidth = "80px"; //弹出框 标题长度
+const tableForm = ref({});
+const dialogType = ref("add");
 //监控数据
 watch(queryInput, (val) => {
   if (val.length > 0) {
-    tableData.value = tableData.value.filter(item => item.name.toLowerCase().match(val.toLowerCase()));
+    tableData.value = tableData.value.filter((item) =>
+      item.name.toLowerCase().match(val.toLowerCase())
+    );
   } else {
     tableData.value = tableDataCopy;
-  } 
-})
+  }
+});
 //选择事件
 const handleSelectionChange = (val) => {
   multipleSelection.value = [];
-  val.forEach(item => {
-    multipleSelection.value.push(item.id)
+  val.forEach((item) => {
+    multipleSelection.value.push(item.id);
   });
-}
+};
 //添加按钮
 const handleAdd = (val) => {
-  tableForm.value = {}
-  dialogType.value = "add"
-  dialogFormVisible.value = true
-}
+  tableForm.value = {};
+  dialogType.value = "add";
+  dialogFormVisible.value = true;
+};
 //编辑按钮
 const handleEdit = (val) => {
-  tableForm.value = val
-  dialogType.value = "edit"
-  dialogFormVisible.value = true
-}
+  tableForm.value = val;
+  dialogType.value = "edit";
+  dialogFormVisible.value = true;
+};
 
 //删除一条按钮
 const handleRowDel = (id) => {
-  console.log(id)
-  let index = tableData.value.findIndex(item => item.id === id)
-  tableData.value.splice(index, 1)
-}
+  console.log(id);
+  let index = tableData.value.findIndex((item) => item.id === id);
+  tableData.value.splice(index, 1);
+};
 
 //删除多条
 const handleListDel = () => {
-  multipleSelection.value.forEach(id => {
-    handleRowDel(id)
-  })
+  multipleSelection.value.forEach((id) => {
+    handleRowDel(id);
+  });
   multipleSelection.value = [];
-}
+};
 
 //确认按钮
 const dialogConfirm = () => {
-  if (dialogType.value === 'add') {
+  if (dialogType.value === "add") {
     tableData.value.push({
       id: tableData.value.length + 1,
-      ...tableForm.value
-    })
+      ...tableForm.value,
+    });
   } else {
-    let index = tableData.value.findIndex(item => item.id === tableForm.id);
-    tableData[index] = tableForm
+    let index = tableData.value.findIndex((item) => item.id === tableForm.id);
+    tableData[index] = tableForm;
   }
-  dialogFormVisible.value = false
-}
+  dialogFormVisible.value = false;
+};
 onMounted(() => {
-  axios.get('https://node222.onrender.com/data').then(res => {
-    if(res.status !== 200) {
-      return alert('请求失败')
+  axios.get("https://node222.onrender.com/data").then((res) => {
+    if (res.status !== 200) {
+      return alert("请求失败");
     }
-    const {data} = res.data
+    const { data } = res.data;
     console.log(data);
-    tableData.value = data
-  })
-})
-
+    tableData.value = data;
+  });
+});
 </script>
 
 <style>
